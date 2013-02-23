2d3f010b054ec6e0a1d2f6379854a59930cfcf4a
bubi-luka <reg.luka@bubi.si>
2013-02-23 19:40:01 +0100

    PRVI POSKUS OBARVANJA PROGRAMA V  BOLJ ELEGANTNE BARVE
    #105

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	varnost app(argc, argv);
 
 	// create style for the application
-/*
+
 	QString pot_do_stilske_datoteke = QApplication::applicationDirPath();
 	QFile datoteka(pot_do_stilske_datoteke + "/style.css");
 
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	QString StyleSheet = QLatin1String(datoteka.readAll());
 
 	qApp->setStyleSheet(StyleSheet);
-*/
+
 	// create signal-slot connections
 	GlavnoOkno *varnost_GlavnoOkno = new GlavnoOkno;
 	QObject::connect(&app, SIGNAL(id_changed()), varnost_GlavnoOkno, SLOT(varnost_id_changed()));
